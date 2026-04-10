215af7f3306e449a7426c4e74bb732a2ba9e7433
João Eduardo <joaoeduardosantos3108@gmail.com>
2026-04-10 11:29:20 -0300

    Primeira versão da pilha(Historico das Url) com alguns testes básicos

---
+++ historico.py
@@ -0,0 +1,45 @@
+#Início da formação do histórico do browser
+
+class Historico:
+    def __init__(self):
+        self.__historico = []
+    #Adcionar uma url
+    def adcionar(self, url):
+        self.__historico.append(url)
+    #Retira a url
+    def remover(self):
+        self.__historico.pop()
+        if self.historico_vazio() == True:
+            raise IndexError('Não é possível remover, histórico vazio')
+    #Demonstrar qual url atual
+    def url_atual(self):
+        if self.historico_vazio() == True:
+            raise ValueError('Histórico Vazio')
+        topo = self.__historico[-1]
+        return topo
+    #Indicar se o histórioco está vazio
+    def historico_vazio(self)-> bool:
+        if len(self.__historico) == 0:
+            return True
+        else:
+            return False
+    #Formatar saída
+    '''Isso Retorna na forma de pilha tradicional, sendo a url mais acima a mais nova que foi adcionada e a mais abaixo foi a primeira a ser adcionanda'''
+    def exibir_historico(self) -> any:
+        for url in self.__historico[::-1]:
+            print(f'[{url}]')
+    
+    def __str__(self):
+        return "".join(f'[{url}]' for url in self.__historico)
+teste = Historico()
+
+teste.adcionar('www.teste.com')
+teste.adcionar('www.youtube.com')
+teste.adcionar('www.ifpb.com')
+teste.adcionar('www.google.com')
+teste.adcionar('www.globo.com')
+
+# teste.remover()
+
+teste.exibir_historico()
+
