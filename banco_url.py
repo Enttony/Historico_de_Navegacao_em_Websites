61d2f5b2760be59886e21d1ae95e8816d60665bc
joaoantonio-dev <tontjoao@gmail.com>
2026-04-11 22:22:28 -0300

    desbugando essa merda

--- banco_url.py
+++ banco_url.py
@@ -1,13 +1,35 @@
 class banco_url:
     def __init__ (self):
-        self.__url = []
+        self.__banco_url = {}
 
-    def ler_arquivo(self, caimho:str):
+    def ler_arquivo(self, caminho:str):
         try:
-            open with(caminho,"r") as arquivo:
-            for x in arquivo:
-                url = x.strip()
-                if url:
-                    self.__url.append(url)
+            with open (caminho,"r") as arquivo:
+                for linha in arquivo:
+                    linha = linha.strip()
+                    if not linha:
+                        continue
+
+                    linha_partida = linha.split()
+                    home = linha_partida[0]
+                    subpaginas = linha_partida[1:]
+
+                    if self.formato_valido(home) == True:
+                        self.__banco_url[home] = subpaginas
+
+                    
         except FileNotFoundError:
             raise FileNotFoundError(f'O Arquivo {caminho} não foi encontrado')
+    
+    def formato_valido(self, url: str) -> bool:
+        return (url.startswith("www.")) or (url.startswith("http://")) or (url.startswith())
+
+
+    def url_existe(self, url) ->bool:
+        for home, subpaginas in self.__banco_url.items():
+            if url == home:
+                return True
+            for sub in subpaginas:
+                if url == home + sub:
+                    return True
+            return False
